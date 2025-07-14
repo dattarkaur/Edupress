4a8e5cdaca1dac6c42fefc84987288d2e269b4c1
pactice-git <practice@gmail.com>
2025-07-14 11:27:21 +0530

    changed

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'Education' );
+//define( 'DB_NAME', 'Education' );
 
 /** Database username */
 define( 'DB_USER', 'dattar' );
