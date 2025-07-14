4dc67fe4cdbb9ca08d65edbb747d10c29a9883e0
inder kaur <inder@gmail.com>
2025-07-14 17:56:12 +0530

    inder

--- wp-config.php
+++ wp-config.php
@@ -23,7 +23,7 @@
 //define( 'DB_NAME', 'Education' );
 
 /** Database username */
-define( 'DB_USER', 'dattar' );
+define( 'DB_USER', 'dattar inder' );
 
 /** Database password */
 define( 'DB_PASSWORD', '123456' );
