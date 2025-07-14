4bcd783c3a9df95d7a6b11a1408dcc3f29846cea
pactice-git <practice@gmail.com>
2025-07-14 17:56:46 +0530

    changed

--- wp-config.php
+++ wp-config.php
@@ -23,7 +23,7 @@
 //define( 'DB_NAME', 'Education' );
 
 /** Database username */
-define( 'DB_USER', 'dattar inder' );
+define( 'DB_USER', 'dattar' );
 
 /** Database password */
 define( 'DB_PASSWORD', '123456' );
